a5ce821c9856cff8885a571c791960d7dca04f4e
Rickard Andersson <gonz@severnatazvezda.com>
2021-05-10 05:56:23 +0000

    finished state

--- src/Library.hs
+++ src/Library.hs
@@ -1,17 +1,20 @@
 module Library where
 
 import RIO
-import RIO.ByteString (ByteString)
 import qualified RIO.ByteString as ByteString
 import qualified RIO.ByteString.Lazy as LazyByteString
+import qualified RIO.Directory as Directory
+import qualified RIO.List as List
+import qualified System.Environment as Environment
 import System.IO (putStrLn)
 import qualified System.Process.Typed as Process
 
 data GitStatus
-  = UnstagedChanges
+  = UnstagedChanges FilePath
   | NotGitRepository
-  | Clean
-  | NoCommits
+  | Clean FilePath
+  | NoCommits FilePath
+  | UnknownStatus FilePath ProcessOutput
   deriving (Eq, Show)
 
 data ProcessOutput = ProcessOutput
@@ -34,7 +37,65 @@ newtype CommandString = CommandString String
 
 runMain :: IO ()
 runMain = do
-  putStrLn "Hello, World!"
+  arguments <- Environment.getArgs
+  case arguments of
+    [path] -> do
+      gitStatuses <- getGitStatuses path
+      let unstaged =
+            foldr
+              ( \status pathsSoFar -> case status of
+                  UnstagedChanges p -> p : pathsSoFar
+                  _otherwise -> pathsSoFar
+              )
+              []
+              gitStatuses
+              & List.sort
+      forM_ unstaged putStrLn
+    _anythingElse ->
+      error "Need a path to search"
+
+getGitStatuses :: FilePath -> IO [GitStatus]
+getGitStatuses path = do
+  localGitStatus <- getGitStatus path
+  case localGitStatus of
+    Clean _path -> pure [Clean path]
+    UnstagedChanges _path -> pure [UnstagedChanges path]
+    NoCommits _path -> pure [NoCommits path]
+    unknown@(UnknownStatus _ _) -> pure [unknown]
+    NotGitRepository -> do
+      subDirectories <- getSubDirectories path
+      foldMap getGitStatuses subDirectories
+
+getSubDirectories :: FilePath -> IO [FilePath]
+getSubDirectories path = do
+  files <- Directory.listDirectory path
+  let completeFilePaths = fmap (path </>) files
+  filterM Directory.doesDirectoryExist completeFilePaths
+
+(</>) :: FilePath -> FilePath -> FilePath
+p1 </> p2 = dropEndingSlashes p1 <> "/" <> p2
+
+dropEndingSlashes :: FilePath -> FilePath
+dropEndingSlashes = List.dropWhileEnd (== '/')
+
+getGitStatus :: FilePath -> IO GitStatus
+getGitStatus path = do
+  processOutput@ProcessOutput
+    { standardOut = (OutputBytes outBytes),
+      standardError = (ErrorBytes errorBytes)
+    } <-
+    getProcessOutput (WorkingDirectory path) (CommandString "git status")
+  if
+      | "Changes not staged for commit" `ByteString.isInfixOf` outBytes ->
+        pure $ UnstagedChanges path
+      | "No commits yet" `ByteString.isInfixOf` outBytes ->
+        pure $ NoCommits path
+      | "fatal: not a git repository (or any of the parent directories): .git"
+          `ByteString.isInfixOf` errorBytes ->
+        pure NotGitRepository
+      | "nothing to commit, working tree clean" `ByteString.isInfixOf` outBytes ->
+        pure $ Clean path
+      | otherwise -> pure (UnknownStatus path processOutput)
 
 getProcessOutput :: WorkingDirectory -> CommandString -> IO ProcessOutput
 getProcessOutput (WorkingDirectory workingDirectory) (CommandString commandString) = do
